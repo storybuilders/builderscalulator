b4456ba008c0677a38b9cac5b45a74841664f7a4
storybuilders <shelbypaget@gmail.com>
2025-07-30 18:50:47 -0600

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+// app/layout.tsx
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
